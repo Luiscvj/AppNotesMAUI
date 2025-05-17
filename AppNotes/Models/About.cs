4c5d44efb5cf677a8e61dac3f2bffa0ee5851f97
Luiscvj <Luiscarlos.villanjurado@gmail.com>
2025-05-17 10:51:46 -0500

    feat: models added

---
+++ AppNotes/Models/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNotes.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+
+        public string Version => AppInfo.VersionString;
+
+        public string MoreInfoUrl => "https://thecodercave.com";
+
+        public string Message => "This app is written in MAUI";
+    }
+}
